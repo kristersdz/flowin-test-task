a104fc9df739cb7f1bef8b328bc2e63700a2dcd4
Kristers Dzintars <faceree123@gmail.com>
2020-08-06 15:13:39 +0300

    added hook to fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { useAxiosGet } from './Hooks/HttpRequests'
 
 function App() {
+  const path = '/wp-json/wp/v2/portfolio/';
+  let portfolio = useAxiosGet(path);
+  let content = null;
+
+  if(portfolio.data){
+    content = portfolio.data.map((post) => 
+      <div>
+        <h4>{post.title.rendered}</h4>
+        <p>{post.acf.page__title_secondary}</p>
+        <p>{post.content.rendered}</p>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <h3>Posts</h3>
+      {content}
     </div>
   );
 }
---
+++ src/Hooks/HttpRequests.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export function useAxiosGet(path) {
+    let domain = 'http://18.158.173.83';
+    let fullPath = domain + path;
+
+    const [request, setRequest] = useState({
+        data: null,
+        loading: false,
+        error: false
+    })
+
+    useEffect(() => {
+        setRequest({
+            data: null,
+            loading: true,
+            error: false
+        })
+        axios.get(fullPath)
+            .then(response => {
+                setRequest({
+                    data: response.data,
+                    loading: false,
+                    error: false
+                })
+            })
+            .catch(() => {
+                setRequest({
+                    data: null,
+                    loading: false,
+                    error: true
+                })
+            })
+    }, [fullPath])
+
+    return request
+}
